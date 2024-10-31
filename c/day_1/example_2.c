a5b71f09963afa031f8785a113a05e5a0f3529ff
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-10-31 21:59:17 +0530

    Feat: basic program

---
+++ c/day_1/example_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  int num = 10;
+  float fnum = 10.00;
+  char ch = '#';
+  char name[5] = "c@#1";
+
+  printf("Number: %d", num);
+  printf("\nFloat Number: %f", fnum);
+  printf("\nCharacter: %c", ch);
+  printf("\nString: %s", name);
+
+  return 0;
+}
